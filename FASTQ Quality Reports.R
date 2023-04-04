1ea346207363124619178c10f225a87d85942e44
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-04-04 12:38:36 +0800

    Update FASTQ Quality Reports.R

--- FASTQ Quality Reports.R
+++ FASTQ Quality Reports.R
@@ -48,3 +48,67 @@ fqlist <- seeFastq(fastq=fastq, batchsize=800, klength=8) # For real data set ba
 seeFastqPlot(fqlist)
 
 
+
+#Filtering and Trimming FASTQ Files with ShortRead
+
+fqtrim <- trimLRPatterns(Rpattern="GCCCGGGTAA", subject=fq)
+sread(fq)[1:2] # Before trimming
+
+
+sread(fqtrim)[1:2] # After trimming
+
+
+#Read counting and duplicate removal 
+
+tables(fq)$distribution # Counts read occurences
+
+
+sum(srduplicated(fq)) # Identifies duplicated reads
+
+
+fq[!srduplicated(fq)]
+
+
+#Trimming low quality tails
+
+cutoff <- 30
+cutoff <- rawToChar(as.raw(cutoff+33))
+sread(trimTails(fq, k=2, a=cutoff, successive=FALSE))[1:2]
+
+
+#Removal of reads with Phred scores below a threshold value
+
+cutoff <- 30
+qcount <- rowSums(as(quality(fq), "matrix") <= 20) 
+fq[qcount == 0] # Number of reads where all Phred scores >= 20
+
+
+#Removal of reads with x Ns and/or low complexity segments
+
+filter1 <- nFilter(threshold=1) # Keeps only reads without Ns
+filter2 <- polynFilter(threshold=20, nuc=c("A","T","G","C")) # Removes reads with nucleotide bias, >=20 of any base
+filter <- compose(filter1, filter2)
+fq[filter(fq)]
+
+
+
+#Memory Efficient FASTQ Processing
+#Streaming through FASTQ files with FastqStreamer and random sampling reads with FastqSampler
+
+fq <- yield(FastqStreamer(fastq[1], 50)) # Imports first 50 reads 
+fq <- yield(FastqSampler(fastq[1], 50)) # Random samples 50 reads 
+
+
+#Streaming through a FASTQ file while applying filtering/trimming functions and writing the results to a new file here SRR038845.fastq_sub in data directory.
+
+f <- FastqStreamer(fastq[1], 50) 
+while(length(fq <- yield(f))) {
+    fqsub <- fq[grepl("^TT", sread(fq))] 
+    writeFastq(fqsub, paste(fastq[1], "sub", sep="_"), mode="a", compress=FALSE)
+}
+close(f)
+
+
+
+
+
